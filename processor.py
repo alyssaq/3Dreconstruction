ef5e3aa84489e538fd6f8ae727d7d8fcea529eb8
alyssaq <alyssaquek@gmail.com>
2017-05-21 13:19:53 +0800

    sphinx comment formatting

--- processor.py
+++ processor.py
@@ -1,31 +1,32 @@
 import numpy as np
 
+
 def read_matrix(path, astype=np.float64):
-  with open(path, 'r') as f:
-    arr = []
-    for line in f:
-      arr.append([(token if token != '*' else -1) for token in line.strip().split()])
-    return np.asarray(arr).astype(astype)
+    with open(path, 'r') as f:
+        arr = []
+        for line in f:
+            arr.append([(token if token != '*' else -1)
+                        for token in line.strip().split()])
+        return np.asarray(arr).astype(astype)
+
 
 def cart2hom(arr):
-  """ Convert catesian to homogenous points 
-    by appending a row of 1s
-    Input shape: d x n
-    Output shape: (d+1) x n """
-  if arr.ndim == 1:
-    return np.hstack([arr, 1])
-  # arr has shape (dimension x num_points)
-  return np.vstack([arr, np.ones(arr.shape[1])])
+    """ Convert catesian to homogenous points by appending a row of 1s
+      :param arr: array of shape (num_dimension x num_points)
+      :returns: array of shape ((num_dimension+1) x num_points) 
+    """
+    if arr.ndim == 1:
+        return np.hstack([arr, 1])
+    return np.vstack([arr, np.ones(arr.shape[1])])
+
 
 def hom2cart(arr):
-  """ Convert homogenous to catesian points
-    by dividing each row by the last row 
-    Input shape: d x n
-    Output shape: (d-1) x n iff d > 1 """
-  # arr has shape: dimensions x num_points
-  num_rows = len(arr)
-  if num_rows == 1 or arr.ndim == 1:
-    return arr
+    """ Convert homogenous to catesian by dividing each row by the last row
+      :param arr: array of shape (num_dimension x num_points)
+      :returns: array of shape ((num_dimension-1) x num_points) iff d > 1 """
+    # arr has shape: dimensions x num_points
+    num_rows = len(arr)
+    if num_rows == 1 or arr.ndim == 1:
+        return arr
 
-  return arr[:num_rows - 1] / arr[num_rows - 1]
-
+    return arr[:num_rows - 1] / arr[num_rows - 1]
